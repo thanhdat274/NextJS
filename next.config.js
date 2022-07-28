f87a7802c9d31c78cb40223e24aefb82e58aa4cd
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-07-29 00:27:02 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// };
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["fakestoreapi.com", "randomuser.me"],
+    hostname: ["fakestoreapi.com", "randomuser.me"],
+  },
+  pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
+};
+
+// module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+      // For the best performance and to avoid false positives,
+      // be as specific as possible with your content configuration.
+    ],
+    theme: {
+      extends: {},
+    },
+    plugins: [],
+  };
+  
