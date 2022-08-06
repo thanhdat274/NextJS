9ed5dd55015e2cc6261d3fe8104e64d6715afb2d
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-08-06 19:34:29 +0700

    update

---
+++ pages/posts/add.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface AddPostPageProps {
+}
+
+export default function AddPostPage(props: AddPostPageProps) {
+    return (
+        <div>
+            Thêm mới post
+        </div>
+    );
+}
---
+++ pages/posts/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface  ListPostPageProps {
+}
+
+export default function ListPostPage (props:  ListPostPageProps) {
+  return (
+    <div>
+      List Post Page
+    </div>
+  );
+}
